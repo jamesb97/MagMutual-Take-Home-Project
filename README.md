09db5b2e2bed6b88dc031cfb8c3c805458dbf9c6
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-01-23 12:02:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## MAG Mutual Take Home Project
+
+#### Take Home Project
+
+At MagMutual, we're continually evolving our platforms and creating new end-to-end services and experiences for our users. This project represents a smaller, focused example of the type of work we regularly take on.
+
+For this exercise, we’d like you to build a service with endpoints that return the provided data, along with a simple UI to display that data. Feel free to be creative in your solution, we are excited to see what you come up with.
+
+We have provided a CSV containing user information; we would like you to build the following endpoints:
+
+- Endpoint to return a specific user (and all associated information)
+- Endpoint to return a list of users created between a date range
+- Endpoint to return a list of users based on a specific profession
+- Custom Endpoint that you design on your own.
+
+Assumptions:
+You can build it in any language/framework combination that you like, or feel is best.
+
+Deliverables:
+The artifacts or link to the source code.
+Answer the following questions:
+
+- What did you think of the project?
+- What didn’t you like about the project?
+- How would you change the project or approach?
+- Anything else you would like to share?
---
+++ index.js
@@ -0,0 +1,89 @@
+// Create a node server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello MagMutual!");
+});
+
+// Read the data from the csv file
+const csvFilePath = "./data.csv";
+const csv = require("csvtojson");
+let jsonObj = [];
+
+csv()
+    .fromFile(csvFilePath)
+    .then((data) => {
+        jsonObj = data;
+        console.log("Data loaded from CSV");
+    });
+
+// Create an endpoint to return all users
+// app.get("/users", (req, res) => {
+//     res.json(jsonObj);
+// });
+
+// Create an endpoint to return a list of users
+app.get("/users", (req, res) => {
+    res.json(jsonObj);
+});
+
+// Create a custom search endpoint
+app.get("/users/search", (req, res) => {
+    const { name } = req.query;
+    if (!name) {
+        return res.status(400).json({ error: "Name query parameter is required" });
+    }
+    const users = jsonObj.filter((user) => 
+        user.first_name.toLowerCase().includes(name.toLowerCase()) || 
+        user.last_name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(users);
+});
+
+// Create an endpoint to return a list of users based on a specific profession
+app.get("/users/profession/:profession", (req, res) => {
+    const profession = req.params.profession;
+    const users = jsonObj.filter((user) => user.profession.toLowerCase() === profession.toLowerCase());
+    res.json(users);
+});
+
+// Create an endpoint to return a list of users created between a date range
+app.get("/users/date-range/:startDate/:endDate", (req, res) => {
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+    const users = jsonObj.filter((user) => {
+        const createdAt = new Date(user.created_at);
+        return createdAt >= new Date(startDate) && createdAt <= new Date(endDate);
+    });
+    res.json(users);
+});
+
+// Create an endpoint to return a specific user
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = jsonObj.find((user) => user.user_id === userId);
+    res.json(user);
+});
+
+// Create a custom endpoint
+app.get("/custom", (req, res) => {
+    const customData = {
+        message: "This is a custom endpoint",
+    };
+    res.json(customData);
+});
+
+//Create a custom endpoint to insert a new user
+app.post("/users", (req, res) => {
+    const user = req.body;
+    jsonObj.push(user);
+    res.json(user);
+});
+
+app.listen(port, () => {
+    console.log(`MagMutual app listening on port ${port}`);
+});
